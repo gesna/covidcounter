70ec41e00016d126f6606838a6a798a7d106a028
Mitchel Huang <t-mih@microsoft.com>
2020-06-27 19:24:43 -0400

    first commit in mitchel

--- app.py
+++ app.py
@@ -8,19 +8,3 @@ table_service = TableService(account_name='covidcountstorage', account_key='NE7x
 @app.route('/')
 def hello_world():
     return 'Hello world'
-
-#add a row to the table 
-def add_entity(StoreName, ID, ZipCode, MaxCapacity, Address, CurrentOccupancy, DateTime, IP):
-    row = Entity()
-    row.PartitionKey = StoreName
-    row.RowKey = ID
-    row.ZipCode = ZipCode
-    row.MaxCapacity = MaxCapacity
-    row.Address = Address
-    row.CurrentOccupancy = CurrentOccupancy
-    row.DateTime = DateTime
-    row.IP = IP 
-    table_service.insert_entity('StoreOccupancy', row)
-
-def get_entity(StoreName, ID):
-    return table_service.get_entity('StoreOccupancy', StoreName, ID)
