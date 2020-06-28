7beb9385c5560cfe1a5a8114eab25a0c77a04e6e
Gesna Aggarwal <t-gesnaa@microsoft.com>
2020-06-27 20:23:39 -0400

    Table Storage connection base, add + get methods implemented

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
 from flask import Flask
+from azure.cosmosdb.table.tableservice import TableService
+from azure.cosmosdb.table.models import Entity
 
 app = Flask(__name__)
+table_service = TableService(account_name='covidcountstorage', account_key='NE7xyUWKIFiRnUowiB3VPwlT6O5bJn1qxhQtSnUvHtubqnd8MbOFdnMLY385DXAPkc32SkxwWf+Kx++D9mjykQ==')
 
 @app.route('/')
 def hello_world():
     return 'Hello world'
+
+#add a row to the table 
+def add_entity(StoreName, ID, ZipCode, MaxCapacity, Address, CurrentOccupancy, DateTime, IP):
+    row = Entity()
+    row.PartitionKey = StoreName
+    row.RowKey = ID
+    row.ZipCode = ZipCode
+    row.MaxCapacity = MaxCapacity
+    row.Address = Address
+    row.CurrentOccupancy = CurrentOccupancy
+    row.DateTime = DateTime
+    row.IP = IP 
+    table_service.insert_entity('StoreOccupancy', row)
+
+def get_entity(StoreName, ID):
+    return table_service.get_entity('StoreOccupancy', StoreName, ID)
