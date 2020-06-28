7055f67788e53461831d05cc36997b0b5d37caeb
Mitchel Huang <mithuang@umich.edu>
2020-06-28 05:54:01 -0400

    actual working back end

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from azure.cosmosdb.table.tableservice import TableService
 from azure.cosmosdb.table.models import Entity
 from datetime import datetime
